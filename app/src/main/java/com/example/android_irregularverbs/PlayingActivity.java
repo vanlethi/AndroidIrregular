6793b95b75aea76e08038d2721b7d254f1415f49
vanlethi <van.le@student.passerellesnumeriques.org>
2019-10-23 11:57:39 +0700

    finish handle

--- app/src/main/java/com/example/android_irregularverbs/PlayingActivity.java
+++ app/src/main/java/com/example/android_irregularverbs/PlayingActivity.java
@@ -5,14 +5,13 @@ import androidx.room.Room;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.DialogInterface;
-import android.media.MediaPlayer;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,6 +23,7 @@ import java.util.List;
 
 public class PlayingActivity extends AppCompatActivity {
     AppDatabase db;
+    int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,56 +40,90 @@ public class PlayingActivity extends AppCompatActivity {
         new AsyncTask<Void, Void, Void>() {
             @Override
             protected Void doInBackground(Void... voids) {
-//                MediaPlayer ring= MediaPlayer.create(PlayingActivity.this,R.drawable.);
-//                ring.start();
                 final List<ListQuestion> questions = db.userDao().getAll();
                 final TextView text_view_question = findViewById(R.id.text_view_question);
                 final LinearLayout liner_layout_list = findViewById(R.id.liner_layout_list);
+                final TextView text_view_indexquestion = findViewById(R.id.text_view_indexquestion);
+                final TextView text_view_timer = findViewById(R.id.text_view_timer);
                 final AlertDialog.Builder alert_message= new AlertDialog.Builder(PlayingActivity.this);
                 alert_message.setTitle("Irregular Game Message");
 
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        text_view_question.setText(questions.get(index_question).question);
-                        String[] list_answer = questions.get(index_question).list_answer.split(", ");
-                        for(int i =0; i<list_answer.length; i++){
-                            final Button btn_answer = new Button(PlayingActivity.this);
-                            btn_answer.setText(list_answer[i]);
-                            btn_answer.setId(i);
-                            btn_answer.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View view) {
-                                    if(btn_answer.getText().equals(questions.get(index_question).correct_answer)){
-                                        alert_message.setMessage("Correct Answer");
-                                        alert_message.setIcon(R.drawable.icon_smile);
-                                    }else {
-                                        alert_message.setMessage("Incorrect Answer");
-                                        alert_message.setIcon(R.drawable.icon_bad);
-                                    }
-                                    alert_message.setPositiveButton("Next question", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialogInterface, int i) {
-                                            if(index_question > questions.size()){
-                                                Toast.makeText(getApplicationContext(), "Het cau hoi roi nhe", Toast.LENGTH_LONG).show();
-                                            }else {
-                                                liner_layout_list.removeAllViews();
-                                                getAndDisplayListquestion(index_question +1);
-                                            }
-                                        }
-                                    }).show();
+                        if(index_question < questions.size()){
+                            final CountDownTimer countDownTimer = new CountDownTimer(10000, 1000){
+                                public  void onTick(long millisUntilFinished){
+                                    text_view_timer.setText("Timer" + millisUntilFinished / 1000);
                                 }
-                            });
-                            liner_layout_list.addView(btn_answer);
+                                @Override
+                                public void onFinish() {
+                                    alert_message.setMessage("Time done");
+                                    alert_message.setIcon(R.drawable.icon_bad);
+                                    alert_message.show();
+                                }
+                            }.start();
+                            text_view_question.setText(questions.get(index_question).question);
+                            text_view_indexquestion.setText("Question:"+(index_question+1)+"/"+questions.size());
+                            String[] list_answer = questions.get(index_question).list_answer.split(", ");
+                            for(int i =0; i<list_answer.length; i++){
+                                final Button btn_answer = new Button(PlayingActivity.this);
+                                btn_answer.setText(list_answer[i]);
+                                btn_answer.setId(i);
+                                btn_answer.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        if(btn_answer.getText().equals(questions.get(index_question).correct_answer)){
+                                            alert_message.setMessage("Correct Answer");
+                                            alert_message.setIcon(R.drawable.icon_smile);
+                                            count ++;
+                                        }else {
+                                            alert_message.setMessage("Incorrect Answer");
+                                            alert_message.setIcon(R.drawable.icon_bad);
+                                        }
+                                        alert_message.setPositiveButton("Next question", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                    countDownTimer.cancel();
+                                                    text_view_timer.setText("");
+                                                    liner_layout_list.removeAllViews();
+                                                    getAndDisplayListquestion(index_question +1);
+                                            }
+                                        }).show();
+                                    }
+                                });
+                                liner_layout_list.addView(btn_answer);
+                            }
+                        }else {
+                            alert_message.setMessage("Question Passed: "+ count);
+                            alert_message.setPositiveButton("Go back", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    Intent intent = new Intent(PlayingActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                }
+                            }).show();
                         }
-                        Toast.makeText(PlayingActivity.this, "Size: " + questions.size(), Toast.LENGTH_SHORT).show();
                     }
                 });
                 return null;
             }
         }.execute();
     }
-
+//    public void setTimer(final TextView text_view_timer, final AlertDialog.Builder alert_message){
+//
+//        new CountDownTimer(10000, 1000){
+//            public  void onTick(long millisUntilFinished){
+//                text_view_timer.setText("" + millisUntilFinished / 1000);
+//            }
+//            @Override
+//            public void onFinish() {
+//                alert_message.setMessage("Time done");
+//                alert_message.setIcon(R.drawable.icon_bad);
+//                alert_message.show();
+//            }
+//        }.start();
+//    }
     public void addListQuestion() {
 
         new AsyncTask<Void, Void, Void>() {
@@ -100,15 +134,15 @@ public class PlayingActivity extends AppCompatActivity {
                 question.list_answer = "begun, began, begin, begon";
                 question.correct_answer= "began";
                 db.userDao().insertAll(question);
-                question.question = "Fill the correct answer for this sentence 'Have you ______________ your lost dog yet?'";
+                question.question = "Fill the correct answer: 'Have you ___ your lost dog yet?'";
                 question.list_answer = "find, found, both are true, both are fail";
                 question.correct_answer= "found";
                 db.userDao().insertAll(question);
-                question.question = "Fill the correct answer for this sentence 'She spoke too softly. I couldn't ______________ her'";
+                question.question = "Fill the correct answer: 'She spoke too softly. I couldn't ___ her'";
                 question.list_answer = "hear, heard, both are true, both are fail";
                 question.correct_answer= "hear";
                 db.userDao().insertAll(question);
-                question.question = "Fill the correct answer for this sentence 'We went shopping and I ______________ a new pair of jeans'";
+                question.question = "Fill the correct answer: 'We went shopping and I ___ a new pair of jeans'";
                 question.list_answer = "win, won, both are true, both are fail";
                 question.correct_answer= "won";
                 db.userDao().insertAll(question);
